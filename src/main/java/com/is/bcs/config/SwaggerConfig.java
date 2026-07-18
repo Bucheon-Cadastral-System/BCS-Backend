aa387ba17ad16b93c9c2611d63d46c7c38befcf0
inwoohub <dlsdndls@naver.com>
2026-07-18 19:29:02 +0900

    #2 [Feature] : Security Config, application.yml 카카오 OAuth2 플로우 구축 & 의존성 정리

---
+++ src/main/java/com/is/bcs/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.is.bcs.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Item API")
+                        .description("Item Application API Documentation")
+                        .version("v1.0"))
+
+                .addServersItem(new Server()
+                        .url("http://localhost:8080/item")
+                        .description("[Default] local 8080 포트"))
+
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(SECURITY_SCHEME_NAME));
+
+    }
+}
